6a40e71bfec02d77d9d662df26b00b56db682481
Tarek Ibrahim <tibrahim@bts.com.eg>
2022-09-04 09:17:50 +0200

    update

--- scripts/ValidateService.sh
+++ scripts/ValidateService.sh
@@ -4,17 +4,18 @@
 
 #get database name
 read database_name < scripts/database-name
-echo "@ `date '+%F %H:%M:%S'` ran $0"
+echo "@ `date '+%F %H:%M:%S'` ran $0" >> /tmp/trace.txt
 if [ -n "$database_name" ]; then
     #Install modules
     while read module
     do
-        echo "@ `date '+%F %H:%M:%S'` from the while loop ran $0"
+        echo "@ `date '+%F %H:%M:%S'` from the while loop ran $0" >> /tmp/trace.txt
+
         
         sudo python3 /odoo/odoo-server/odoo-bin --db_user=odoo_update --db_password='odoo_update' --log-level=error --addons-path=/odoo/odoo-server/addons,/odoo/custom/addons --xmlrpc-port=7777 --stop-after-init --database=$database_name --update=$module --db_host "127.0.0.1"
     done < /odoo/custom/scripts/upgrade-modules
 fi
 
 #Start Odoo
-echo "@ `date '+%F %H:%M:%S'` restarting the service"
+echo "@ `date '+%F %H:%M:%S'` restarting the service" >> /tmp/trace.txt
 sudo systemctl restart odoo-server.service
