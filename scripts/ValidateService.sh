4507318a580ad21b331da49c58ad23cef52d5a0b
Tarek Ibrahim <tibrahim@bts.com.eg>
2022-09-04 09:43:41 +0200

    uuu

--- scripts/ValidateService.sh
+++ scripts/ValidateService.sh
@@ -16,7 +16,7 @@ if [ -n "$database_name" ]; then
 
         
         sudo python3 /odoo/odoo-server/odoo-bin --db_user=odoo_update --db_password='odoo_update' --log-level=error --addons-path=/odoo/odoo-server/addons,/odoo/custom/addons --xmlrpc-port=7777 --stop-after-init --database=$database_name --update=$module --db_host "127.0.0.1"
-    done < /odoo/custom/scripts/upgrade-modules
+    done < "$dirname"/upgrade-modules
 fi
 
 #Start Odoo
